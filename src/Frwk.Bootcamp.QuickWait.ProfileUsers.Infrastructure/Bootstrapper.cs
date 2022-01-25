e25eb618701ab0a5fbf3d1f2f1e7d33c11a8c5ad
Fernando Nogueira <fgeraldo11@gmail.com>
2022-01-25 10:58:34 -0300

    Configuração do Repositório de usuário e mapeamento de usuário com endereço

---
+++ src/Frwk.Bootcamp.QuickWait.ProfileUsers.Infrastructure/Bootstrapper.cs
@@ -0,0 +1,17 @@
+﻿using Frwk.Bootcamp.QuickWait.ProfileUsers.Domain.Contracts;
+using Frwk.Bootcamp.QuickWait.ProfileUsers.Infrastructure.UserContext;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Frwk.Bootcamp.QuickWait.ProfileUsers.Infrastructure
+{
+    public static class Bootstrapper
+    {
+        public static IServiceCollection AddRepositoriesProfileUsers(this IServiceCollection services)
+        {
+            services
+                .AddScoped<IUserRepository, UserRepository>();
+
+            return services;
+        }
+    }
+}
---
+++ src/Frwk.Bootcamp.QuickWait.ProfileUsers.Infrastructure/UserContext/UserMap.cs
@@ -0,0 +1,14 @@
+﻿using Frwk.Bootcamp.QuickWait.ProfileUsers.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Frwk.Bootcamp.QuickWait.ProfileUsers.Infrastructure.UserContext
+{
+    public class UserMap : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.HasKey(x => x.Id);
+        }
+    }
+}
